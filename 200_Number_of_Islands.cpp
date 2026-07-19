85bb053886fe3f41b6c993b011f32766cafaf7b1
Deepak <boradeepakreddy2005@gmail.com>
2026-07-19 11:27:03 +0530

    Sync: 200. Number of Islands (Medium) - Leet2Git

--- 200_Number_of_Islands.cpp
+++ 200_Number_of_Islands.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    void dfs(int i, int j, int n, int m, vector<vector<char>> &grid, vector<vector<bool>> &visited) {
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0' || visited[i][j]) {
+    void dfs(int i, int j, int n, int m, vector<vector<char>> &grid) {
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
             return;
         } 
 
-        visited[i][j] = true;
+        grid[i][j] = '0';
 
-        dfs(i + 1, j, n, m, grid, visited);
-        dfs(i - 1, j, n, m, grid, visited); 
-        dfs(i, j + 1, n, m, grid, visited); 
-        dfs(i, j - 1, n, m, grid, visited);
+        dfs(i + 1, j, n, m, grid);
+        dfs(i - 1, j, n, m, grid); 
+        dfs(i, j + 1, n, m, grid); 
+        dfs(i, j - 1, n, m, grid);
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-
         int ans = 0;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(i, j, n, m, grid, visited);
+                if(grid[i][j] == '1') {
+                    dfs(i, j, n, m, grid);
                     ans++;
                 }
             }
