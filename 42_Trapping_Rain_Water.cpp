56e0ac191fe5482031ad93425bd92d05ae11f211
Deepak <boradeepakreddy2005@gmail.com>
2026-07-21 23:40:34 +0530

    Sync: 42. Trapping Rain Water (Hard) - Leet2Git

---
+++ 42_Trapping_Rain_Water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        int ans = 0;
+
+        vector<int> prefixMax(n);
+        vector<int> suffixMax(n);
+
+        prefixMax[0] = height[0];
+        for(int i = 1; i < n; i++) {
+            prefixMax[i] = max(height[i], prefixMax[i - 1]);
+        }
+
+        suffixMax[0] = height[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+            suffixMax[i] = max(height[i], suffixMax[i + 1]);
+        }
+
+        for(int i = 1; i < n - 1; i++) {
+            if(i < prefixMax[i] && i < suffixMax[i]) {
+                ans += min(prefixMax[i], suffixMax[i]) - height[i];
+            }
+        }
+
+        return ans;
+    } 
+};
